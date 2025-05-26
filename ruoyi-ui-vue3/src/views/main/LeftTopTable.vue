39a15f233423a0e4562dd76115a45e428c51ed52
Thomaz <994708568@qq.com>
2025-05-26 23:14:14 +0800

    报警检测

--- ruoyi-ui-vue3/src/views/main/LeftTopTable.vue
+++ ruoyi-ui-vue3/src/views/main/LeftTopTable.vue
@@ -6,47 +6,97 @@
         实时数据
       </span>
     </div>
-    <div>
-      <el-table :data="tableData" style="width: 100%" :row-class-name="tableRowClassName">
-        <el-table-column type="index" width="50" />
-        <el-table-column prop="date" label="名称"  width="60" />
-        <el-table-column prop="name" label="PH"width="60" />
-        <el-table-column prop="address" label="温度" width="60"/>
-        <el-table-column prop="date" label="水位" width="60"/>
-        <el-table-column prop="name" label="电位" />
-        <el-table-column prop="address" label="溶解氧"width="70" />
-        <el-table-column prop="date" label="电导率"width="70" />
-        <el-table-column prop="name" label="氨氮" width="60"/>
-        <el-table-column prop="address" label="位置" width="60"/>
-      </el-table>
-    </div>
+    <el-table :data="tableData" style="width: 100%" :row-class-name="tableRowClassName">
+      <el-table-column type="index" width="50" />
+      <el-table-column prop="code" label="名称" width="60" />
+      <el-table-column prop="PH" label="PH" width="60" />
+      <el-table-column prop="WD" label="温度" width="60" />
+      <el-table-column prop="SW" label="水位" width="60" />
+      <el-table-column prop="DW" label="电位" />
+      <el-table-column prop="RJY" label="溶解氧" width="70" />
+      <el-table-column prop="DDL" label="电导率" width="70" />
+      <el-table-column prop="AD" label="氨氮" width="60" />
+      <el-table-column prop="lng" label="位置" width="60" />
+    </el-table>
   </div>
 
 </template>
 <script setup>
 
-  const tableData = [
+  const tableData = ref([
     {
-      date: '名称',
-      name: 'Tom',
-      address: 'Tom',
+      index: 1,
+      code: "J15",
+      WD: '60°',
+      SW: "60",
+      PH: '7',
+      DW: '20',
+      RJY: '15',
+      DDL: '8',
+      AD: '30',
+      lng: "680.8",
     },
     {
-      date: '名称',
-      name: 'Tom',
-      address: 'Tom',
+      index: 2,
+      code: "J21",
+      WD: '50°',
+      SW: "40",
+      PH: '8.6',
+      DW: '10',
+      RJY: '35',
+      DDL: '80',
+      AD: '14',
+      lng: "650.8",
     },
     {
-      date: '名称',
-      name: 'Tom',
-      address: 'Tom',
+      index: 3,
+      code: "J54",
+      WD: '58°',
+      SW: "40",
+      PH: '8',
+      DW: '10',
+      RJY: '13',
+      DDL: '12',
+      AD: '20',
+      lng: "610.8",
     },
     {
-      date: '名称',
-      name: 'Tom',
-      address: 'Tom',
+      index: 4,
+      code: "J28-1",
+      WD: '60°',
+      SW: "60",
+      PH: '7',
+      DW: '20',
+      RJY: '15',
+      DDL: '8',
+      AD: '30',
+      lng: "610.8",
     },
-  ]
+    {
+      index: 5,
+      code: "J45",
+      WD: '44°',
+      SW: "55",
+      PH: '8.6',
+      DW: '60',
+      RJY: '15',
+      DDL: '11',
+      AD: '31',
+      lng: "640.3",
+    },
+    {
+      index: 6,
+      code: "J47",
+      WD: '30°',
+      SW: "40",
+      PH: '7.5',
+      DW: '28',
+      RJY: '12',
+      DDL: '17',
+      AD: '20',
+      lng: "610.8",
+    },
+  ]);
 </script>
 
 <style lang="scss" scoped>
@@ -54,6 +104,7 @@
     width: 604px;
     padding: 10px;
     background: url('@/assets/static/left.png') no-repeat;
+    /* background-size: 100% 100%; */
 
     .title {
       height: 42px;
@@ -65,25 +116,5 @@
         display: inline-block;
       }
     }
-  }
-
-  :deep() {
-    .el-table tr {
-      background-color: rgba(21, 85, 75, 0.2);
-    }
-
-    .el-table {
-      --el-table-bg-color: none;
-      --el-table-text-color: white;
-      --el-table-border-color: none;
-      --el-table-row-hover-bg-color: none
-    }
-
-    .el-table .el-table__header-wrapper th,
-    .el-table .el-table__fixed-header-wrapper th {
-      background-color: #155548 !important;
-      color: white;
-    }
-
   }
 </style>
---
+++ ruoyi-ui-vue3/src/views/main/RightBottomTable.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="table">
+    <div class="title">
+      <img src="@/assets/static/subTitle.png" alt="">
+      <span>
+        报警列表
+      </span>
+    </div>
+    <el-table :data="tableData" style="width: 100%" :row-class-name="tableRowClassName">
+      <el-table-column type="index" label="序号" width="55" />
+      <el-table-column prop="date" label="监测井" />
+      <el-table-column prop="name" label="指标参数" width="55" />
+      <el-table-column prop="address" label="标准值" />
+      <el-table-column prop="name1" label="监测值" />
+      <el-table-column prop="address1" label="超标倍率" />
+    </el-table>
+  </div>
+</template>
+<script setup>
+  import { onMounted, reactive, toRaw, ref, nextTick } from "vue";
+
+  const tableData = [
+    {
+      date: '检测井1',
+      name: 'PH',
+      address: '6.7',
+      name1: '7',
+      address1: '105%',
+    },
+    {
+      date: '检测井2',
+      name: '温度',
+      address: '42',
+      name1: '30',
+      address1: '70%',
+    },
+    {
+      date: '检测井3',
+      name: '氨氮',
+      address: '1',
+      name1: '0.8',
+      address1: '80%',
+    },
+    {
+      date: '检测井4',
+      name: '水位',
+      address: '60',
+      name1: '80',
+      address1: '120%',
+    },  
+     {
+      date: '检测井5',
+      name: 'PH',
+      address: '6.7',
+      name1: '7',
+      address1: '105%',
+    },
+         {
+      date: '检测井6',
+      name: '水位',
+      address: '60',
+      name1: '40',
+      address1: '80%',
+    },
+  ]
+</script>
+
+<style lang="scss" scoped>
+  .table {
+    width: 604px;
+    padding: 10px;
+    background: url('@/assets/static/left.png') no-repeat;
+    background-color: rgba(9, 21, 42, 0.8);
+
+    .title {
+      height: 42px;
+      line-height: 42px;
+      display: flex;
+
+      span {
+        font-size: 18px;
+        display: inline-block;
+      }
+    }
+  }
+</style>
